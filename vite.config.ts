0a8f2cb6e76b35d13cea806223ac9e2e8b0dc941
harshit7-wq <harshitah039@gmail.com>
2025-09-02 14:01:47 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,20 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// Change this if your repo name is different
+const repoName = 'fitness-tracker-app 1'
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // In dev it serves from '/', but on GitHub Pages it serves from '/<repo>/'
+  base: mode === 'development' ? '/' : `/${repoName}/`,
   plugins: [
     react(),
-    legacy()
+    legacy(),
   ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
-})
+  },
+}))
